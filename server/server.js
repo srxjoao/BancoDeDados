fa4bf77873606fff2de5947af38efeb4e939ba67
srxjoao <joao.barbosa6@estudante.ifms.edu.br>
2024-09-29 22:33:43 -0400

    v0.0..6

--- server/server.js
+++ server/server.js
@@ -1,24 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+//o poll vem do pacote node PG qual esa sendo utilizado 
 const { Pool } = require('pg');
+
+const app = express();
+const port = 5001;
+
+app.use(express.json());
+app.use(cors()); 
+
 //Aki começa a conexão do banco de dados com o expresse com a senha do meu postgre,usuário,localhost e o banco 
 const pool = new Pool({
   connectionString: "postgres://postgres:1402@localhost:5432/projeto",
 });
 
-//O connect e utilizado para estabelecer a conexão  jumtamento com o POOL metódo mais eficaz(atualmete) 
-//o poll vem do pacote node PG qual esa sendo utilizado 
-pool.connect((err, client, done) => {
-  if (err) {
-    return console.error('Error fetching client from pool', err);
+// Rota para a raiz
+app.get('/usuarios', (req, res) => {
+  res.send('Servidor está funcionando! Acesse /usuarios para listar os usuários.');
+});
+
+// Rota para listar usuários
+app.get('/', async (req, res) => {
+  try {
+      //O metodo query apresenta os dados do banco,ele executa comandos SQL no exemplo utilizei o * para listar todos os dados dos usuários
+    const result = await pool.query('SELECT * FROM projeto_bancodedados');
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Erro ao buscar usuários:', error);
+    res.status(500).json({ error: 'Erro ao buscar usuários' });
   }
-  
-  //O metodo query apresenta os dados do banco,ele executa comandos SQL no exemplo utilizei o * para listar todos os dados dos usuários
-  client.query('SELECT * FROM projeto_bancodedados', (err, result) => {
-    done(); 
-    if (err) {
-      return console.error('Error running query', err);
-    }
-    
-    // Exibe os resultados da consulta
-    console.log(result.rows);
-  });
+});
+
+// Inicia o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
